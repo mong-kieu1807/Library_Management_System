68e1cb29836b1e2dc68dbe3fb5ec4e4115546184
Thảo Nguyên <0306231166@caothang.edu.vn>
2026-06-18 21:52:17 +0700

    Implement private user management API endpoints

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,12 @@ Route::prefix('v1/auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+Route::middleware('auth:sanctum')->prefix('private/v1')->group(function () {
+    Route::get('/users', [App\Http\Controllers\Admin\UserManagementController::class, 'index']);
+    Route::post('/users', [App\Http\Controllers\Admin\UserManagementController::class, 'store']);
+    Route::get('/users/{id}', [App\Http\Controllers\Admin\UserManagementController::class, 'show']);
+    Route::patch('/users/{id}', [App\Http\Controllers\Admin\UserManagementController::class, 'update']);
+    Route::delete('/users/{id}', [App\Http\Controllers\Admin\UserManagementController::class, 'destroy']);
+    Route::post('/users/{id}/reset-password', [App\Http\Controllers\Admin\UserManagementController::class, 'resetPassword']);
+});
