bdbae22b131cf89fe8bd56e27a8a14fb64604449
Thảo Nguyên <0306231166@caothang.edu.vn>
2026-06-18 21:39:55 +0700

    fix api login

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     protected $primaryKey = 'role_id';
+    public $timestamps = false; // Bảng roles không có cột created_at/updated_at trong migration
+
+    protected $fillable = [
+        'role_name',
+        'description',
+    ];
 
     public function users()
     {
