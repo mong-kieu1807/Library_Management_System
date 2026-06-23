72b3cbb2cea50beaa2a4b42fde13c088a59b8b2c
Nam <0306231408@caothang.edu.vn>
2026-06-23 17:47:40 +0700

    Sửa lỗi xung đột

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReservationController;
-use App\Http\Controllers\BorrowController;
-
-Route::post('/reservations', [ReservationController::class, 'reserveBook']);
-
-Route::get(
-    '/reservations/user/{userId}',
-    [ReservationController::class, 'getUserReservations']
-);
-
-Route::delete(
-    '/reservations/{reservationId}',
-    [ReservationController::class, 'cancelReservation']
-);
-
-Route::get(
-    '/borrowings/current/{userId}',
-    [BorrowController::class, 'currentBorrowings']
-);
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\LibraryCardController;
